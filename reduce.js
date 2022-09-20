47050b3607728de3ec4d5e05ae1ecf3ab6c4ec97
Dave Fisher <dave.j.fish@gmail.com>
2022-09-20 11:21:27 -0700

    all reduce tests passing

--- reduce.js
+++ reduce.js
@@ -2,7 +2,12 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((arr, item) => {
+    for (const shoe of item.shoes) arr.push(shoe);
+    return arr;
+  }, []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
